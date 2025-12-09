3ce0cce6e5aadf24238ccaa6e2dd4b2ab5c7deb6
dev-dx9 <dev.dx9@gmail.com>
2025-12-10 00:35:26 +0300

    feat(models): add Hotels model structure

--- src/database.py
+++ src/database.py
@@ -1,6 +1,11 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
 
 engine = create_async_engine(settings.DB_URL)
+
+
+class Base(DeclarativeBase):
+    pass
