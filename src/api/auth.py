0684e9ac20baa098d7be0c77f80103a184c71dc6
dev-dx9 <dev.dx9@gmail.com>
2026-01-27 14:19:18 +0300

    refactor(auth): move authentication logic to AuthService

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,31 +1,23 @@
 from fastapi import APIRouter, HTTPException, Response
-from pwdlib import PasswordHash
-import jwt
-from datetime import datetime, timedelta, timezone
 
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
 from src.schemas.users import UserRequestAdd, UserAdd
+from src.services.auth import AuthService
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
 
-password_hash = PasswordHash.recommended()
-SECRET_KEY = '09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7'
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+@router.post('/register')
+async def register_user(data: UserRequestAdd):
 
+    hashed_password = AuthService().password_hash.hash(data.password)
+    new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
+    async with async_session_maker() as session:
+        await UsersRepository(session).add(new_user_data)
+        await session.commit()
 
-def verify_password(plain_password, hashed_password):
-    return password_hash.verify(plain_password, hashed_password)
-
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({'exp': expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    return {'status': 'ok'}
 
 
 @router.post('/login')
@@ -35,20 +27,8 @@ async def login_user(data: UserRequestAdd, response: Response):
         if not user:
             raise HTTPException(status_code=401, detail='User with that email does not register')
 
-        if not verify_password(data.password, user.hashed_password):
+        if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail='Incorrect password')
-        access_token = create_access_token({'user_id': user.id})
+        access_token = AuthService().create_access_token({'user_id': user.id})
         response.set_cookie('access_token', access_token)
         return {'access_token': access_token}
-
-
-@router.post('/register')
-async def register_user(data: UserRequestAdd):
-
-    hashed_password = password_hash.hash(data.password)
-    new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
-    async with async_session_maker() as session:
-        await UsersRepository(session).add(new_user_data)
-        await session.commit()
-
-    return {'status': 'ok'}
